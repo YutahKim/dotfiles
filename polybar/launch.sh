46ec5fa1593449782eefbc178050e85838d24f94
YutahKim <hectordiaz4iv7@gmail.com>
2025-03-22 13:31:40 -0600

    Fixed picom issue by restoring the monitor ID changed from 1 to 0 because of the gtx installed drivers

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -9,5 +9,6 @@ killall -q polybar
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+polybar bar3 2>&1 | tee -a /tmp/polybar3.log & disown
 
 echo "Bars launched..."
