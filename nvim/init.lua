4a17f65f03f8f9a397913eef48cb17b7e71d62b8
YutahKim <hectordiaz4iv7@gmail.com>
2025-04-15 23:58:21 -0600

    Added a better cmd bar to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -389,6 +389,24 @@ require('lazy').setup({
    event = "VeryLazy",
 },
 
+--Better command line
+-- lazy.nvim
+{
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+    }
+},
+
   -- Start screen
   {
   'goolord/alpha-nvim',
