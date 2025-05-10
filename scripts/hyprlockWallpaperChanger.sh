a1e624ace1ea29bc851fcb70d9be933c108acbaf
YutahKim <hectordiaz4iv7@gmail.com>
2025-05-10 10:53:08 -0600

    Updated waybar config for battery section

--- scripts/hyprlockWallpaperChanger.sh
+++ scripts/hyprlockWallpaperChanger.sh
@@ -2,5 +2,5 @@
 # Save this script as ~/scripts/random_wallpaper.sh
 
 WALLPAPER_DIR="$HOME/Pictures/bg"
-find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.webp" \) | shuf -n 1
+find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) | shuf -n 1
 
