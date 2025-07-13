26527689b0f95abf389eaba2847bd54515793d17
YutahKim <hectordiaz4iv7@gmail.com>
2025-07-13 11:57:56 -0600

    Updated nvim store

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -537,6 +537,20 @@ require('lazy').setup({
     cmd = { "Typr", "TyprStats" },
 },
 
+{
+  "alex-popov-tech/store.nvim",
+  dependencies = {
+    "OXY2DEV/markview.nvim", -- optional, for pretty readme preview / help window
+  },
+  cmd = "Store",
+  keys = {
+    { "<leader>s", "<cmd>Store<cr>", desc = "Open Plugin Store" },
+  },
+  opts = {
+    -- optional configuration here
+  },
+},
+
   -- Start screen
   {
   'goolord/alpha-nvim',
