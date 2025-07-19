73a7e3db7d5f94ceb9cd2d216476c1998efa9543
YutahKim <hectordiaz4iv7@gmail.com>
2025-07-18 20:05:52 -0600

    nvim config updated

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -110,7 +110,7 @@ require('lazy').setup({
       config = function()
           local lspconfig = require('lspconfig')
           local capabilities = require('cmp_nvim_lsp').default_capabilities()
-          
+
           -- Common on_attach function
           local on_attach = function(client, bufnr)
               local bufopts = { noremap=true, silent=true, buffer=bufnr }
@@ -488,17 +488,17 @@ require('lazy').setup({
 },
 
 {
-    "kawre/leetcode.nvim",
-    build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
-    dependencies = {
-        "nvim-telescope/telescope.nvim",
-        -- "ibhagwan/fzf-lua",
-        "nvim-lua/plenary.nvim",
-        "MunifTanjim/nui.nvim",
-    },
-    opts = {
-        -- configuration goes here
-    },
+  "kawre/leetcode.nvim",
+  build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+  dependencies = {
+      "nvim-telescope/telescope.nvim",
+      -- "ibhagwan/fzf-lua",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+  },
+  opts = {
+      -- configuration goes here
+  },
 },
 {
   "lervag/wiki.vim",
@@ -507,18 +507,6 @@ require('lazy').setup({
     -- wiki.vim configuration goes here, e.g.
   end
 },
-
--- {"vimwiki/vimwiki",
---   init = function()
---       vim.g.vimwiki_list = {
---           {
---           path = '~/docs/vimwiki',
---           syntax = 'markdown',
---           ext = '.md',
---           },
---       }
---   end,
--- },
 
 --Markdown render
 {
@@ -562,56 +550,4 @@ require('lazy').setup({
   end
   },
 
-  -- Dashboard (Alpha)
--- {
---   "goolord/alpha-nvim",
---   dependencies = { "nvim-tree/nvim-web-devicons" },
---   config = function()
---     local alpha = require("alpha")
---     local dashboard = require("alpha.themes.dashboard")
---     local project = require("telescope").extensions.project
-
---     dashboard.section.header.val = {
---       "    ███╗   ██╗██╗   ██╗███╗   ███╗",
---       "    ████╗  ██║██║   ██║████╗ ████║",
---       "    ██╔██╗ ██║██║   ██║██╔████╔██║",
---       "    ██║╚██╗██║██║   ██║██║╚██╔╝██║",
---       "    ██║ ╚████║╚██████╔╝██║ ╚═╝ ██║",
---       "    ╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝",
---     }
-
---     dashboard.section.buttons.val = {
---       dashboard.button("SPC f r", "  Recently opened files", "<cmd>Telescope oldfiles<cr>"),
---       dashboard.button("SPC p", "  Projects", "<cmd>Telescope projects<cr>"),
---       dashboard.button("SPC f d", "  Open Dotfiles", "<cmd>edit ~/.config<cr>"),
---       dashboard.button("SPC s l", "  Restore Last Session", "<cmd>lua require('persistence').load({ last = true })<cr>"),
---       dashboard.button("q", "  Quit", "<cmd>qa<cr>"),
---     }
-
---     dashboard.section.footer.val = "Neovim is my code editor"
---     dashboard.opts.opts.noautocmd = true
---     alpha.setup(dashboard.opts)
---   end
--- },
-
--- -- Session Persistence
--- {
---   "folke/persistence.nvim",
---   event = "BufReadPre",
---   config = function()
---     require("persistence").setup({
---       dir = vim.fn.stdpath("data") .. "/sessions/",
---       options = { "buffers", "curdir", "tabpages", "winsize" },
---     })
---   end,
--- },
-
--- -- Project switcher
--- {
---   "ahmedkhalf/project.nvim",
---   config = function()
---     require("project_nvim").setup()
---     require("telescope").load_extension("projects")
---   end,
--- },
 })
